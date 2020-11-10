9e9a8e1f687409759111fa3b2732701058a449b0
Godzilla14 <74238136+Godzilla14@users.noreply.github.com>
2020-11-10 14:12:38 +0100

    Update dataStatistics.py

--- methods/dataStatistics.py
+++ methods/dataStatistics.py
@@ -30,13 +30,13 @@ def inputNumber(prompt):
 
 
 DESCRIPTIONS = { #Dictionary with description of each statistic
-    "Mean Temperature": "Mean (average) Temperature.",
-    "Mean Growth rate": "Mean (average) Growth rate.",
-    "Std Temperature": "Standard deviation of Temperature.",
-    "Std Growth rate": "Standard deviation of Growth rate.",
-    "Rows": "The total number of rows in the data.",
-    "Mean Cold Growth rate": "Mean (average) Growth rate when Temperature is less than 20 degrees.",
-    "Mean Hot Growth rate": "Mean (average) Growth rate when Temperature is greater than 50 degrees.",
+    1 : "Mean (average) Temperature.",
+    2 : "Mean (average) Growth rate.",
+    3 : "Standard deviation of Temperature.",
+    4 : "Standard deviation of Growth rate.",
+    5 : "The total number of rows in the data.",
+    6 : "Mean (average) Growth rate when Temperature is less than 20 degrees.",
+    7 : "Mean (average) Growth rate when Temperature is greater than 50 degrees.",
 }
     
     
@@ -89,33 +89,35 @@ def dataStatistics(data, statistic):
         temperatures = [item[0] for item in data]  # returns the first columm in data
         growth_rates = [item[1] for item in data]  # returns the second columm in data
     
-    if statistic == 'Mean Temperature':  # calculates the average temperature with inbuilt function
+    if statistic == 1 :  # calculates the average temperature with inbuilt function
         return st.mean(temperatures)
         
-    elif statistic == 'Mean Growth rate':  # calculates the average growth rate with inbuilt function
+    elif statistic == 2 :  # calculates the average growth rate with inbuilt function
        return st.mean(growth_rates)
        
-    elif statistic == 'Std Temperature':  # calculates the standart deviation of temperature
+    elif statistic == 3 :  # calculates the standart deviation of temperature
         return st.stdev(temperatures)
         
-    elif statistic == 'Std Growth rate':  # calculates the standart deviation of growth
+    elif statistic == 4:  # calculates the standart deviation of growth
         return st.stdev(growth_rates)
         
-    elif statistic == 'Rows':  # returns the lenght of array or iterable
+    elif statistic == 5 :  # returns the lenght of array or iterable
         return len(data)
         
-    elif statistic == 'Mean Cold Growth rate':
+    elif statistic == 6 :
         # takes growth rate from data if temperature in the same row is < 20 then calculates the average 
         # bacteria is not mentioned because it is not important for this calculation 
         cold_growth_rates = [growth_rate for temperature, growth_rate, _ in data if temperature < 20]
         return st.mean(cold_growth_rates)
         
-    elif statistic == 'Mean Hot Growth rate':
+    elif statistic == 7 :
         # takes growth rate from data if temperature in the same row is > 50 then calculates the average
         # bacteria is not mentioned because it is not important for this calculation 
         hot_growth_rate = [growth_rate for temperature, growth_rate, _ in data if temperature > 50]
         return st.mean(hot_growth_rate)
 
+    else:
+        print(statistic, "does not exist")
     
     
     
