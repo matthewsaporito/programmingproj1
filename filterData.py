dde73a91d72b8274f70c8bc7a713f52d60c639f1
sterlie <ssterlie@hotmail.com>
2020-11-10 13:19:48 +0100

    comments for filterdata

--- filterData.py
+++ filterData.py
@@ -14,11 +14,11 @@ def filterData(prompt, data):
     print("Filters:")
     print("1. Filter bacteria")
     print("2. Filter grwoth rate")
-    print("3. Return to main menu")
+    print("3. Return to main menu")#prints menu for filtering data.
         
     while True:
         
-        selection =int(input("Please enter on of the above options:" ))
+        selection =int(input("Please enter on of the above options:" ))#takes user input 
 
         try:
             
@@ -30,14 +30,14 @@ def filterData(prompt, data):
                 print("3. Listeria")
                 print("4.Brochothrix thermosphacta")
 
-                bacteria = int(input("please choose an option, 1 to 4:"))
+                bacteria = int(input("please choose an option, 1 to 4:"))#if user input is 1, bacteria-menu is displayed.
     
             
-                if bacteria == 1 or bacteria == 2 or bacteria == 3 or bacteria == 4:
+                if bacteria == 1 or bacteria == 2 or bacteria == 3 or bacteria == 4:#filters for bacteria
                 
-                    data = data[ data[:,2]== bacteria,:]
-                else:
-                    print("Please type a number: 1, 2, 3 or 4")  
+                    data = data[ data[:,2]== bacteria,:]#overwrites data with new filter for bacteria
+                else:           
+                    print("Please type a number: 1, 2, 3 or 4")#prints message if user input dosn't fufill requriments.
                     
                     return data  
             
@@ -46,20 +46,21 @@ def filterData(prompt, data):
                 print("Please specify the range of growthrate")
                 lower = float(input("Min growth rate:"))
                 upper = float(input("Max growth rate:"))
+                #takes min and max growth rate as input 
                 try:
-                    data = data[ data[:,1]<upper and data[:,1]>lower,:]
+                    data = data[ data[:,1]<upper and data[:,1]>lower,:]#overwrites data with new filter for growthrate
                 except ValueError:
-                      print("Please enter a number as value for the minimun and maixmun growth rate")
+                      print("Please enter a number as value for the minimun and maixmun growth rate")#prints message if user input dosn't fufill requriments.
                       
                       return data 
                 
     
             elif selection == 3:
                 
-                showMenu()
+                showMenu()#returns to mainmenu if user input is 3. 
                 
         except ValueError:
-                print("Please choose an options: 1,2 or 3")
+                print("Please choose an options: 1,2 or 3")#prints message if Valueerror occurs in filter-data menu. 
     
   
 
--- main_script.py
+++ main_script.py
@@ -18,19 +18,18 @@ data = None
 
 
 showMenu()
-
+#displays main menu.
 
 while True:
     
-    selection=int(input("Please enter one of the above options:" )) 
+    selection=int(input("Please enter one of the above options:" )) #asks user for input from utils.
 
     if selection in {2, 3, 4} and not data:
-        print("Please load data first.")
+        print("Please load data first.")#makes sure data is loaded. if not approaching option 2,3 or 4 is not allowed.
         showMenu()
         continue
-        
-        
-    try:
+       
+    try: 
         if selection == 1:
             data = dataLoad()
             
@@ -50,7 +49,7 @@ while True:
             break
         
         else:
-            print("Invalid Choice. PLease enter one of the above options (1, 2, 3, 4 or 5)!")
+            print("Invalid Choice. PLease enter one of the above options (1, 2, 3, 4 or 5)!")#prints if user input is different than 1-5
             
     except ValueError:
         print("please type a number 1-5")
