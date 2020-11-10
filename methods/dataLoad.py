6d12827b3800a88b57d3ff6ed7845279cc626b87
Godzilla14 <74238136+Godzilla14@users.noreply.github.com>
2020-11-10 15:09:57 +0100

    changes

--- methods/dataLoad.py
+++ methods/dataLoad.py
@@ -25,13 +25,17 @@ def filecheck():
     #checks that user file exists loads as 'data'
     filename = str(input("Please enter The file you whish to load:"))
     try:
-        data = np.loadtxt(filename, delimiter=" ")
+        # data = np.loadtxt(filename, delimiter=" ")
+        data = [item.strip().split() for item in open(filename, 'r')]
         return data
     except OSError:
         print("The file could not be found. Please Enter an existing file")
         return filecheck()
+    except Exception as e:
+        print(e)
+        return filecheck()
     
-def validateData(data):
+def validateRowLen(data):
     #checks that data file has 3 columns
     validated = []
     for i, item in enumerate(data):
@@ -45,35 +49,40 @@ def validateData(data):
         except AssertionError as e:
             pass
         
-        validated = data
-    return data
+    return validated
 
 def dataLoad():
     # pulls data from filecheck and filters to keep only valid rows from conditions above
     bdata = filecheck()
-        
-    data = np.array(bdata)
+    
+    data = validateRowLen(bdata)
+    # cast to float, float, int
+    data = [(float(i), float(j), int(k)) for i, j, k in data]
+    data = np.array([np.array(item) for item in data])
+    print(data)
     
     data = data[data[:,0]>10]
     data = data[data[:,0]<60]
     data = data[data[:,1]>0]
     data = data[data[:,2]>=1]
     data = data[data[:,2]<=4]
-    data = validateData(data)
+    
     
     #print(data)
     
     #error values by row, specifying error
-    for i in range(len(bdata)):
-        if bdata[i,0]<10:
+    for i, item in enumerate(bdata):
+        temp, rate, b_type = item 
+        temp, rate, b_type = float(temp), float(rate), int(b_type)
+        if temp<10:
             print("Temp is too low in row {:d}".format(i+1))
-        if bdata[i,0]>60:
+        if temp>60:
             print("Temp is too high in row {:d}".format(i+1))
-        if bdata[i,1]<0:
+        if rate<0:
             print("invalid growth rate in row {:d}".format(i+1))  
-        if not np.any(int(bdata[i,2]) == np.array([1,2,3,4])):
+        if not b_type in (1,2,3,4,5):
             print("invalid bacteria type in row {:d}".format(i+1))
-        if not len(bdata[i]) == 3:
+        if not len(item) == 3:
             print("data has wrong shape at row {i}, expected N x 3, got {size}")
 
     return data
