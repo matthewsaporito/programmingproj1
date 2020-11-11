8b8080a011fe2e688b01c28c112b34595d40f16e
Godzilla14 <74238136+Godzilla14@users.noreply.github.com>
2020-11-11 18:52:37 +0100

    FIX

--- methods/dataPlot.py
+++ methods/dataPlot.py
@@ -64,9 +64,6 @@ def dataPlot(data):
         out = []  # empty list
         
         for n, bacteria_data in bacteria_development.items():  # for every n and its bacteria_data in bacteria_development dictionary
-            bacteria_data = sorted(bacteria_data, key=lambda item: item[0])  # sorts by temperature the growth rate
-            x_vals, y_vals = zip(*bacteria_data)  # creates a tuple of x and y values by unzipping bacteria_data
-            name = bacteria_dictionary[n]  # also assigns a name for n from bacteria_dictionary
             if bacteria_data:
                 bacteria_data = sorted(bacteria_data, key=lambda item: item[0])  # sorts by temperature the growth rate
                 print('DATA', bacteria_data)
@@ -74,6 +71,7 @@ def dataPlot(data):
                 name = bacteria_dictionary[n]  # also assigns a name for n from bacteria_dictionary
                 out.append((x_vals, y_vals, name))  # puts the values into the empty list
             
+        print('OUT', out)
         return out
     
     
