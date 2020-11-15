f80e06bd840bafdf3aef45c41b5b0f7fac55afe2
Godzilla14 <74238136+Godzilla14@users.noreply.github.com>
2020-11-15 16:21:12 +0100

    columms

--- project2/utils.py
+++ project2/utils.py
@@ -2,8 +2,12 @@
 """
 Created on Mon Nov  9 19:22:30 2020
 
-@author: apek
+@author: Anna Pekarova
 """
+
+import numpy as np
+
+
 
 def menu(choices):
     
@@ -18,3 +22,19 @@ def menu(choices):
             return choices[selection]
         else:
             print("Invalid Choice. Please choose one out of", range(1, len(choices)))
+            
+
+def getColumms(grades):
+    try:  # tries indexing columms as np.array (preffered)
+        grades = np.delete(grades, (0), axis=0)   # deletes the first row
+        StudentID = grades[:, 0]  # returns the first columm in grades
+        Name = grades[:, 1]  # returns the second columm in grades
+        Assignment1 = grades[:, 2]  # returns the third columm in grades
+        Assignment2 = grades[:, 3]  # returns the third columm in grades
+        Assignment3 = grades[:, 4]  # returns the third columm in grades
+        
+    except TypeError:  # if error ocurs, uses indexing for iterables (slower than above, if used on np array)
+        grades = grades[1:]
+        StudentID, Name, Assignment1, Assignment2, Assignment3 = zip(*grades) 
+    
+    return StudentID, Name, Assignment1, Assignment2, Assignment3
