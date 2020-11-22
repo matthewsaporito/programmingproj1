833d958f4dee58ca1fd8b47ab9f1d9d2338ff550
Godzilla14 <74238136+Godzilla14@users.noreply.github.com>
2020-11-22 17:10:11 +0100

    filtering nan

--- project2/utils.py
+++ project2/utils.py
@@ -13,6 +13,9 @@ Created on Mon Nov  9 19:22:30 2020
 """
 
 import numpy as np
+import math as m
+from pandas import isnull
+from pandas import NA
 
 
 
@@ -38,14 +41,20 @@ def getColumms(filteredData):
 '''
 
 
-def computeFinalGrades(filteredData): 
-   assignmentGrades = [[*args] for studentIDs, names, *args in filteredData]  # gets list of lists of assignment grades
-   out = []  # creates an empty list
+def computeFinalGrades(data): 
+   assignmentGrades = [assignments for studentID, name, *assignments in data]  # gets list of lists of assignment grades
+   out1 = []  # creates an empty list
    
    for item in assignmentGrades:  # for each list in list of list
-       numberOfAssignments = len([grade for grade in item if grade])  # gets the lenght of of a list in list of lists not counting Nones
-       finalGrade = sum(assignmentGrades)/numberOfAssignments  # gets the final grade for each student
+       Grades = [grade for grade in item if isnull(grade) == False]  # get rid of nans
+       out1.append(Grades)
+      
+   out = []
+   for it in out:
+       numberOfAssignments = len([it])  # gets the lenght of of a list in list of lists not counting Nones
+       finalGrade = sum(it)/numberOfAssignments  # gets the final grade for each student
        out.append(finalGrade)  # saves the final grade into the empty list
    
    gradesFinal = np.array(out)  # changes empty list into a numpy array (I prefer to work with iterables
    return gradesFinal
+
