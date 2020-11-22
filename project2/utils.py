4b363a88ab0c5260798ac48f45c3905e162fd615
Godzilla14 <74238136+Godzilla14@users.noreply.github.com>
2020-11-22 18:08:41 +0100

    finalgrade

--- project2/utils.py
+++ project2/utils.py
@@ -16,6 +16,7 @@ import numpy as np
 import math as m
 from pandas import isnull
 from pandas import NA
+from roundgrade8 import roundGrade
 
 
 
@@ -32,29 +33,40 @@ def menu(choices):
             return choices[selection]
         else:
             print("Invalid Choice. Please choose one out of", range(1, len(choices)))
-            
-'''
-def getColumms(filteredData):
-    filteredData = sorted(filteredData, key = lambda filteredData_entry: filteredData_entry[0])  # sorts the grades by the first columm 
-    studentIDs, names, *args = zip(*filteredData)
-    return studentIDs, names, *args
-'''
 
 
-def computeFinalGrades(data): 
-   assignmentGrades = [assignments for studentID, name, *assignments in data]  # gets list of lists of assignment grades
-   out1 = []  # creates an empty list
+def computeFinalGrades(data):
+    """
+    Computes final grades
+    Parameters:
+        data: iterable that contains (studentID, name, assignment1, ..., assignmentN)
+    Return: 
+        np.array of final grades
+    Author: Anna Pekarova
+    """
+    assignmentGrades = [assignments for studentID, name, *assignments in data]  # gets list of lists of assignment grades
+    out1 = []  # creates an empty list
+    
+    for item in assignmentGrades:  # for each list in list of list
+       Grades1 = [grade for grade in item if isnull(grade) == False]  # get rid of nans
+       Grades = roundGrade(Grades1)  # calls the round function for each row
+       out1.append(Grades)  # adds the values to the list
+    
+    out = []  # creates a new empty list
+    for item in out1:  # for each list in the list out1
+       if -3 in item:  # if there is -3 in the list in the list
+           finalGrade = -3  # then the final grade is -3
+           out.append(finalGrade)  # adds this grade to the empty list
+       else:
+           sortedList = sorted(item)  # sorts the numbers in the list in the list from the smallest up
+           trimmedList = sortedList[1:]  # cuts of the smallest one
+           numberOfAssignments = len(trimmedList)  # gets the lenght of of a list in the list
+           finalGrade = sum(trimmedList)/numberOfAssignments  # gets the final grade for each student
+           out.append(finalGrade)  # saves the final grade into the empty list
    
-   for item in assignmentGrades:  # for each list in list of list
-       Grades = [grade for grade in item if isnull(grade) == False]  # get rid of nans
-       out1.append(Grades)
-      
-   out = []
-   for it in out:
-       numberOfAssignments = len([it])  # gets the lenght of of a list in list of lists not counting Nones
-       finalGrade = sum(it)/numberOfAssignments  # gets the final grade for each student
-       out.append(finalGrade)  # saves the final grade into the empty list
-   
-   gradesFinal = np.array(out)  # changes empty list into a numpy array (I prefer to work with iterables
-   return gradesFinal
+    gradesFinal = np.array(out)  # changes the list into a numpy array
+    return gradesFinal
+    
+    
+    
 
