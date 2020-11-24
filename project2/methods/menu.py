a430b950a5cb15bb3259a919062c93a607e63124
sterlie <ssterlie@hotmail.com>
2020-11-24 18:28:59 +0100

    Update menu.py

--- project2/methods/menu.py
+++ project2/methods/menu.py
@@ -6,8 +6,8 @@ Created on Tue Nov 10 12:15:39 2020
 
 # Menu options presented to user
 def showMenu():
-    print("1. Check for data errors.")
-    print("2. Load new data")
+    print("1. Load new data.")
+    print("2. Check for data errors.")
     print("3. Generate plots")
     print("4. Display list of grades.")
     print("5. Quit")
